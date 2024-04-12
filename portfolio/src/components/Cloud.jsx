12a9e6b0a17f1555967134137f619b2273a84f6d
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-04-12 11:39:19 +0530

    feat:Cloud positions

--- portfolio/src/components/Cloud.jsx
+++ portfolio/src/components/Cloud.jsx
@@ -2,11 +2,13 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
-export function Cloud(props) {
+export function Cloud({opacity, ...props}) {
   const { nodes, materials } = useGLTF('./models/cloud/model.gltf')
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.Mball001.geometry} material={materials.Material} />
+      <mesh geometry={nodes.Mball001.geometry}>
+        <meshStandardMaterial {...materials['lamert2SG.0.01']} transparent opacity={opacity}/>
+      </mesh>
     </group>
   )
 }
