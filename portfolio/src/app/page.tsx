b8a8727577f807396e582765614efc87200953ab
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-04-12 15:42:54 +0530

    feat:Path for airplane

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { Experience } from "@/components/Experience";
+import { Experience } from "@/components/Experience.jsx";
 import { Canvas } from "@react-three/fiber";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
     <>
-      <Canvas>
+      <Canvas camera={{
+        position: [0,0,5],
+        fov: 30
+      }}>
         <color attach= "background" args={["#ececec"]} />
         <Experience />
       </Canvas>
---
+++ portfolio/src/components/Experience.jsx
@@ -0,0 +1,96 @@
+"use client";
+import { Float, Line, OrbitControls } from "@react-three/drei";
+import { Background } from "./Background";
+import { Airplane } from "./Airplane";
+import { Cloud } from "./Cloud";
+import { useMemo } from "react";
+import * as THREE from "three";
+
+const LINE_NB_POINTS = 2000;
+
+function renderClouds() {
+  return (
+    <>
+      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
+      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
+      <Cloud
+        opacity={0.7}
+        scale={[0.3, 0.3, 0.4]}
+        position={[2, -0.2, -2]}
+        rotation-y={Math.PI / 2}
+      />
+      <Cloud
+        opacity={0.7}
+        scale={[0.4, 0.4, 0.4]}
+        position={[1, -0.2, -12]}
+        rotation-y={Math.PI / 9}
+      />
+      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]} />
+      <Cloud opacity={0.3} scale={[0.8, 0.8, 0.8]} position={[0, 1, -100]} />
+    </>
+  );
+}
+
+function CurveParams() {
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+        new THREE.Vector3(0, 0, -80),
+        new THREE.Vector3(0, 0, -90),
+        new THREE.Vector3(0, 0, -100),
+      ],
+      false,
+      "catmullrom",
+      0.5
+    );
+  }, []);
+
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  }, [curve]);
+
+  const shape = useMemo(()=>{
+    const shape = new THREE.Shape();
+    shape.moveTo(0,-0.5);
+    shape.lineTo(0,0.2);
+    return shape;
+  },[curve]);
+
+  return [curve,shape];
+}
+
+
+export const Experience = () => {
+    const [curve,shape] = CurveParams();
+  return (
+    <>
+      <OrbitControls />
+      <Background />
+      <Float floatIntensity={2} speed={2}>
+        <Airplane rotation-y={Math.PI / 2} scale={[0.2, 0.2, 0.2]} />
+      </Float>
+      <group position-y={-2}>
+        <mesh>
+            <extrudeGeometry 
+                args={[shape,{
+                    steps: LINE_NB_POINTS,
+                    bevelEnabled: false,
+                    extrudePath: curve,
+                }]}
+            />
+            <meshStandardMaterial color={"white"} opacity={0.7} transparent />
+        </mesh>
+      </group>
+      {renderClouds()}
+    </>
+  );
+};
