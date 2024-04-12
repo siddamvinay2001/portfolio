b8e31bbcfc3ae5033b37460833949c0688a07b2c
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-04-12 11:29:48 +0530

    feat:Airplane float

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { OrbitControls } from "@react-three/drei"
+import { Float, OrbitControls } from "@react-three/drei"
 import { Background } from "./Background"
 import { Airplane } from './Airplane'
 import {Cloud} from './Cloud'
@@ -9,10 +9,9 @@ export const Experience = ()=>{
         <>
             <OrbitControls />
             <Background />
-            <mesh>
-                <boxGeometry/>
-                <meshNormalMaterial/>
-            </mesh>
+            <Float floatIntensity={2} speed={2}>
+                <Airplane rotation-y = {Math.PI/2} scale = {[0.6,0.6,0.6]}/>
+            </Float>
         </>
     )
 }
