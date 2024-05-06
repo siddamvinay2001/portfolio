e34ec6b557ae5cf625501b343bc3243ab3447bd0
siddamvinay2001 <siddamvinay1368@gmail.com>
2024-05-06 11:24:23 +0530

    Add links and bold fonts

--- portfolio/src/components/TextSection.jsx
+++ portfolio/src/components/TextSection.jsx
@@ -10,17 +10,17 @@ export const TextSection = ({ currentState, setCurrentState }) => {
     {
       id: 1,
       title: "Work Experience",
-      description: `• Software developer-IC2 at Service Now\n  - Led Agile development teams, facilitated onboarding of new team members, conducted code reviews and optimizations.\n• Associate Software developer-IC1 at Service Now\n  - Contributed to team projects, collaborated on feature development, and provided support.\n• Intern at Service Now\n  - Gained hands-on experience in software development processes, assisted with testing, and participated in team meetings.`,
+      description: `• Software engineer IC2 at Service Now\n  - Led end-to-end implementation of a dynamic product details page and designed RESTful APIs for fetching product details.\n  - Implemented pagination on the home page, reducing load time from 120s to 3s and enhancing user experience.\n  - Resolved HTTP errors (400/413) by optimizing database calls and payload sizes, improving system reliability.\n  - Developed an internal testing tool for ServiceNow applications, streamlining testing processes.\n  - Designed and developed a comprehensive dashboard system to visualize collected metrics, providing actionable insights and enhancing data-driven decision-making processes within the organization.`,
     },
     {
       id: 2,
       title: "Education",
-      description: `• B.Tech - National Institute of Technology Warangal\n  Electronics and Communication Engineering\n  Duration: 08/2018 - 06/2022\n  CGPA: 7.55\n• Intermediate -  Sri Chaitanya Junior Kalasala, Chandanagar\n  Math, Physics, Chemistry\n  Duration: 05/2016 - 05/2018\n  Percentage: 97.7%\n• School (SSC) -  Alphores High School\n  Exam Year: 04/2016\n  Grade: 9.8`,
+      description: `• B.Tech - National Institute of Technology Warangal\n  Electronics and Communication Engineering\n  Duration: 08/2018 - 06/2022\n  CGPA: 7.55\n• Intermediate - Sri Chaitanya Junior Kalasala, Chandanagar\n  Math, Physics, Chemistry\n  Duration: 05/2016 - 05/2018\n  Percentage: 97.7%\n• School (SSC) - Alphores High School\n  Exam Year: 04/2016\n  Grade: 9.8`,
     },
     {
       id: 3,
       title: "Projects",
-      description: `• App Manager Revamp (08/2022 - 05/2023)\n- Implemented end-to-end UI revamp for the app manager's home page\n- Created custom reusable components and improved load time from 120 seconds to 3 seconds using pagination\n- Implemented product details page, improved performance by reducing API calls\n - Addressed 404 errors by reducing size of payload\n\n• Next.js Website for Dental Clinic\n - https://saivijyadentalclinic-vc6l.vercel.app/\n- Developed a custom website for a dental clinic showcasing treatments, patient testimonials, etc.\n\n• Dashboard for Major Incident Management (09/2021 - 11/2023)\n- Created Key Performance Indicators, visualizations, and widgets for major incident management\n\n• Artifacts App Manager (06/2022 - 08/2022)\n- Built internal tool for testing app snapshots, reducing time spent on lengthy store review process\n\n• Google reCAPTCHA Widget (05/2021 - 06/2021)\n- Developed custom reCAPTCHA widget for web pages to prevent spam and fraudulent activities`,
+      description: `• Next.js Website for Dental Clinic\n- https://saivijyadentalclinic-vc6l.vercel.app/\n- Developed a custom website for a dental clinic showcasing treatments, patient testimonials, etc.\n\n• Google reCAPTCHA Widget (05/2021 - 06/2021)\n- Developed custom reCAPTCHA widget using ServiceNow platform for web pages to prevent spam and fraudulent activities`,
     },
     {
       id: 4,
@@ -30,12 +30,12 @@ export const TextSection = ({ currentState, setCurrentState }) => {
     {
       id: 5,
       title: "Contact",
-      description: `• Email: siddamvinay1368@gmail.com\n• Github: https://github.com/siddamvinay2001\n• LinkedIn: https://www.linkedin.com/in/vinay-siddam-84051b1a9/`,
+      description: `• Email: siddamvinay1368@gmail.com\n• Github\n- https://github.com/siddamvinay2001\n• LinkedIn\n- https://www.linkedin.com/in/vinay-siddam-84051b1a9/`,
     },
     {
       id: 6,
       title: "Thank you",
-      description: `Hope you enjoyed your journey`,
+      description: `Hope you enjoyed the journey`,
     },
   ];
 
@@ -48,16 +48,29 @@ export const TextSection = ({ currentState, setCurrentState }) => {
     currentState > -1 ? textConfigs[currentState].description : "";
 
   return (
-    <div className="absolute top-20 left-0 max-h-[80%] right-0 z-10 flex flex-col items-center mb-20  justify-center">
+    <div className="absolute top-20 left-20 right-0 max-h-[80%] max-w-[80%] z-10 items-center mb-20 flex flex-col justify-center">
       {currentState > -1 && (
-        <div className="text-center py-4 px-8 overflow-y-auto bg-white max-w-3/5  font-serif text-black rounded-lg">
-          {title.length > 0 && <div className="text-lg">{title}</div>}
+        <div className="text-center py-4 px-8 overflow-y-auto bg-white max-w-3/5 font-serif text-black rounded-lg">
+          {title.length > 0 && <div className="text-lg font-bold">{title}</div>}
           {description.length > 0 && (
             <div className="text-md text-left mt-3">
               {description.split("\n").map((line, index) => (
                 <div key={index}>
                   {line.startsWith("•") ? (
-                    <p>{line}</p>
+                    <p className="font-bold">{line}</p> // Bold bullet points
+                  ) : line.includes("http") ||
+                    line.includes("https") ||
+                    line.includes("://") ? (
+                    <p>
+                      <a
+                        href={line.trim()}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline hover:no-underline"
+                      >
+                        {line.trim()}
+                      </a>
+                    </p>
                   ) : (
                     <span>&nbsp;&nbsp;&nbsp;&nbsp;{line}</span>
                   )}
@@ -68,7 +81,7 @@ export const TextSection = ({ currentState, setCurrentState }) => {
           <button
             className="mt-3 w-8 h-8 text-sm text-red-200 bg-red-500 rounded-lg"
             onClick={() => {
-              if (currentState != 6) setCurrentState(-1);
+              if (currentState !== 6) setCurrentState(-1);
               else window.location.reload();
             }}
           >
